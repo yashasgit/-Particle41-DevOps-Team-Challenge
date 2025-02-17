087fee42aeca6b60842df477991656eb5f7c2600
yashasgit <144605087+yashasgit@users.noreply.github.com>
2025-02-18 02:18:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,17 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_time_and_ip():
-    # Get current timestamp
     timestamp = datetime.utcnow().isoformat() + "Z"
-    
-    # Get visitor's IP address
     ip = request.remote_addr
-    
-    # Return JSON response
     return jsonify({
         "timestamp": timestamp,
         "ip": ip
     })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+# No need for __main__ block when using Gunicorn
+~
