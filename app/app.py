9ff7a49780b32ef1a5e416bd77803e0f280fa9ae
yashasgit <144605087+yashasgit@users.noreply.github.com>
2025-02-18 01:15:42 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_time_and_ip():
-    # Get the current timestamp
+    # Get current timestamp
     timestamp = datetime.utcnow().isoformat() + "Z"
     
-    # Get the visitor's IP address
-    ip_address = request.remote_addr
+    # Get visitor's IP address
+    ip = request.remote_addr
     
-    # Return the JSON response
+    # Return JSON response
     return jsonify({
         "timestamp": timestamp,
-        "ip": ip_address
+        "ip": ip
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
